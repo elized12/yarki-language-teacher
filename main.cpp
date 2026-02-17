d268527d6ad87bcbdb784a52a4d0954ef91e951b
Elized <frolov_k2004@mail.ru>
2026-02-17 16:31:17 +0300

    [FEATURE] Добавлена обработка CORS

--- main.cpp
+++ main.cpp
@@ -13,6 +13,30 @@ int main(int argc, char *argv[])
     }
 
     drogon::app().loadConfigFile(pathConfigJson);
+    drogon::app().registerPostHandlingAdvice(
+        [](const drogon::HttpRequestPtr &request, const drogon::HttpResponsePtr &response)
+        {
+            response->addHeader("Access-Control-Allow-Origin", "*");
+            response->addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response->addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        });
+
+    drogon::app().registerPreRoutingAdvice([](const drogon::HttpRequestPtr &request,
+                                              drogon::FilterCallback &&stop,
+                                              drogon::FilterChainCallback &&pass)
+                                           {
+        if (request->method() != drogon::HttpMethod::Options)
+        {
+            pass();
+            return;
+        }
+
+        auto response = drogon::HttpResponse::newHttpResponse();
+        response->addHeader("Access-Control-Allow-Origin", "*");
+        response->addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response->addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        stop(response); });
+
     drogon::app().run();
 
     return 0;
